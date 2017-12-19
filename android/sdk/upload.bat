de29814630333c282650d7f190cdf9e14c54bb3a
zhaodashuai <zhaodsh@fxiaoke.com>
2017-12-19 17:12:49 +0800

    ci

--- android/sdk/upload.bat
+++ android/sdk/upload.bat
@@ -1,5 +1,5 @@
-copy ..\..\packages\weex-js-framework\index.js .\assets\main.js  && 
-call gradle upload -PcompileType=weex_debug  && 
-copy ..\..\packages\weex-js-framework\index.min.js .\assets\main.js && 
-call gradle upload -PcompileType=weex_release &
+copy ..\..\packages\weex-js-framework\index.js .\assets\main.js  && ^
+call gradle upload -PcompileType=weex_debug  &&  ^
+copy ..\..\packages\weex-js-framework\index.min.js .\assets\main.js && ^
+call gradle upload -PcompileType=weex_release & ^
 pause
