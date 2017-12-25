6925e6b657cdc0b6e626136049dc5ca6ebe5555d
lid <ld0809@163.com>
2017-12-25 15:43:15 +0800

    * [android] 修改toast使用的context

--- android/sdk/src/main/java/com/taobao/weex/ui/module/WXModalUIModule.java
+++ android/sdk/src/main/java/com/taobao/weex/ui/module/WXModalUIModule.java
@@ -30,6 +30,7 @@ import android.widget.Toast;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.taobao.weex.WXEnvironment;
 import com.taobao.weex.WXSDKEngine;
 import com.taobao.weex.annotation.JSMethod;
 import com.taobao.weex.bridge.JSCallback;
@@ -85,7 +86,7 @@ public class WXModalUIModule extends WXSDKEngine.DestroyableModule {
       duration = Toast.LENGTH_SHORT;
     }
     if (toast == null) {
-      toast = Toast.makeText(mWXSDKInstance.getContext(), message, duration);
+      toast = Toast.makeText(WXEnvironment.sApplication, message, duration);
     } else {
       toast.setDuration(duration);
       toast.setText(message);
