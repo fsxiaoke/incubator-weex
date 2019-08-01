ea59bb9376e5b916423a162c1fc7b4a33556c72b
zhaodsh <zhaodsh@fxiaoke.com>
2019-08-01 17:30:59 +0800

    ci

--- android/sdk/src/main/java/com/taobao/weex/WXSDKInstance.java
+++ android/sdk/src/main/java/com/taobao/weex/WXSDKInstance.java
@@ -1674,8 +1674,8 @@ public class WXSDKInstance implements IWXActivityStateListener,View.OnLayoutChan
   public void onRootCreated(WXComponent root) {
     this.mRootComp = root;
     this.mRootComp.mDeepInComponentTree =1;
-    
-    mRenderContainer.addView(root.getHostView(), new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    root.getHostView().setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    mRenderContainer.addView(root.getHostView() );
     setSize(mRenderContainer.getWidth(),mRenderContainer.getHeight());
   }
 
