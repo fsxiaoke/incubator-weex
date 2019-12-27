831119ff88e83ecc44a0e3bfb566938b7100a09d
lid0809 <ld0809@163.com>
2019-12-27 15:38:37 +0800

    bug

--- android/sdk/src/main/java/com/taobao/weex/ui/component/facishare/FsViewsPageAdapter.java
+++ android/sdk/src/main/java/com/taobao/weex/ui/component/facishare/FsViewsPageAdapter.java
@@ -76,7 +76,7 @@ public class FsViewsPageAdapter extends PagerAdapter {
     // PagerAdapter只缓存三张要显示的图片，如果滑动的图片超出了缓存的范围，就会调用这个方法，将图片�?�?
     @Override
     public void destroyItem(ViewGroup view, int position, Object object) {
-        view.removeView(pages.get(position));
+        view.removeView((View)object);
     }
 
     // 当要显示的图片可以进行缓存的时�?�，会调用这个方法进行显示图片的初始化，我们将要显示的ImageView加入到ViewGroup中，然后作为返回值返回即�?
